e0d701637b6f91566a8e8ef7f9e768d4f17c8d81
François Caremoli <francois.caremoli@wanadoo.fr>
2023-06-14 14:09:18 +0200

    Ajout des liens dynamiques

---
+++ src/main/java/com/bigcorp/booking/correction/controller/Camion.java
@@ -0,0 +1,28 @@
+package com.bigcorp.booking.correction.controller;
+
+public class Camion {
+	
+	private Integer id;
+	private String nom;
+	
+	public  Camion(Integer id, String nom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	
+	
+
+}
--- src/main/java/com/bigcorp/booking/correction/controller/MonPremierControleur.java
+++ src/main/java/com/bigcorp/booking/correction/controller/MonPremierControleur.java
@@ -1,8 +1,16 @@
 package com.bigcorp.booking.correction.controller;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MonPremierControleur {
@@ -11,5 +19,30 @@ public class MonPremierControleur {
 	public String getSalut() {
 		return "vue-salut";
 	}
-
+	
+	@ModelAttribute("maValeur")
+	public String getMaValeur() {
+		return LocalDateTime.now().toString();
+	}
+	
+	@ModelAttribute("camion")
+	public Camion getCamion() {
+		Camion camion = new Camion(1,"Volvo");
+		return camion;
+	}
+	
+	@ModelAttribute("camions")
+	public List<Camion> getCamions() {
+		return Arrays.asList(new Camion(1, "Volvo"), new Camion(2, "Renault"));
+	}
+	
+	@RequestMapping(path="/camion",method = RequestMethod.GET )
+	public ModelAndView getCamionParIdAvecParametreDeRequete(@RequestParam(name = "camionId") Integer id) {
+		return new ModelAndView("vue-salut", "camionDynamique", new Camion(id, "monCamion"));
+	}
+	
+	@RequestMapping(path="/camion/{camionId}",method = RequestMethod.GET )
+	public ModelAndView getCamionParIdAvecVariableDansLePath(@PathVariable(name = "camionId") Integer id) {
+		return new ModelAndView("vue-salut", "camionDynamique", new Camion(id, "monCamion"));
+	}
 }

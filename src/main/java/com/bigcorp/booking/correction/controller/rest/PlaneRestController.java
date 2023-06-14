dbd967d77b82723d66a8e4a7947704e3f0c36b5a
François Caremoli <francois.caremoli@wanadoo.fr>
2023-06-14 16:46:12 +0200

    Ajout POST REST

--- src/main/java/com/bigcorp/booking/correction/controller/rest/PlaneRestController.java
+++ src/main/java/com/bigcorp/booking/correction/controller/rest/PlaneRestController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
@@ -25,6 +27,11 @@ public class PlaneRestController {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No plane found");
 		}
 		return plane;
+	}
+	
+	@PostMapping
+	public Plane save(@RequestBody Plane plane) {
+		return planeService.save(plane);
 	}
 
 }
